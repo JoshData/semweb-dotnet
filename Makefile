ceb097709f9478b0f31051bc7e59e514259c9e4f
Joshua Tauberer <jt@occams.info>
2007-01-21 20:44:22 +0000

    0.82 release = 1.0 RC 1

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.811
+VERSION=0.82
     # don't forget to update src/AssemblyInfo.cs!!
 
 all: bin/SemWeb.dll bin/SemWeb.PostgreSQLStore.dll bin/SemWeb.MySQLStore.dll bin/SemWeb.SqliteStore.dll bin/SemWeb.Sparql.dll bin/rdfstorage.exe bin/rdfquery.exe
--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright (c) 2006 Joshua Tauberer <http://razor.occams.info>")]
+[assembly: AssemblyCopyright("Copyright (c) 2007 Joshua Tauberer <http://razor.occams.info>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
-[assembly: AssemblyVersion("0.8.1.1")]
+[assembly: AssemblyVersion("0.8.2.0")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
