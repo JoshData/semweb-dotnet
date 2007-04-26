6b536a7272ba4abb00c8160d8d3171567fe95401
Joshua Tauberer <jt@occams.info>
2007-04-26 18:22:30 +0000

    examples/select.cs: use a FOAF file that actually exists

--- examples/select.cs
+++ examples/select.cs
@@ -4,15 +4,17 @@ using SemWeb;
 public class Select {
 	const string RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 	const string FOAF = "http://xmlns.com/foaf/0.1/";
+
+	const string example_foaf_file = "http://rdfweb.org/people/danbri/rdfweb/danbri-foaf.rdf";
 	
 	static readonly Entity rdftype = RDF+"type";
 	static readonly Entity foafPerson = FOAF+"Person";
 	static readonly Entity foafknows = FOAF+"knows";
 	static readonly Entity foafname = FOAF+"name";
-
+	
 	public static void Main() {
 		Store store = new MemoryStore();
-		store.Import(RdfReader.LoadFromUri(new Uri("http://dannyayers.com/misc/foaf/foaf.rdf")));
+		store.Import(RdfReader.LoadFromUri(new Uri(example_foaf_file)));
 		
 		Console.WriteLine("These are the people in the file:");
 		foreach (Statement s in store.Select(new Statement(null, rdftype, foafPerson))) {
