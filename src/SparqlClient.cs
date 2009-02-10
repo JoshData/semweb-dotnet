da95c6e0c0ce3a456496ea68ddb3b8adce61bc6f
Joshua Tauberer <jt@occams.info>
2009-02-10 21:38:01 +0000

    proper method call for xmlns

--- src/SparqlClient.cs
+++ src/SparqlClient.cs
@@ -641,8 +641,7 @@ namespace SemWeb.Query {
 		}
 		
 		public override void Init(Variable[] variables) {
-			output.WriteStartElement("sparql");
-			output.WriteAttributeString("xmlns", "http://www.w3.org/2005/sparql-results#");
+			output.WriteStartElement("sparql", "http://www.w3.org/2005/sparql-results#");
 			output.WriteStartElement("head");
 			foreach (Variable var in variables) {
 				if (var.LocalName == null) continue;
