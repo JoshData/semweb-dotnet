1001536e475999eee948e139c513c291a0d6c52f
Ruben <ruben@example.org>
2010-03-08 16:05:43 +0000

    Correction last commit message: "AdvancedN3Writer" instead of "AdvancedN3Parser". Added XML documentation to Constants.cs.

--- src/Constants.cs
+++ src/Constants.cs
@@ -1,22 +1,31 @@
 namespace SemWeb.Constants
 {
-	public static class Prefix
-	{
+    /// <summary>
+    /// Common RDF namespaces.
+    /// </summary>
+    public static class Namespace
+    {
         public const string RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
         public const string LOG = "http://www.w3.org/2000/10/swap/log#";
- 	}
-	
-	public static class Identifier
-    {
-        public static readonly Entity RdfNil = Prefix.RDF + "nil";
     }
-	
-	public class Predicate
-    {
-        public static readonly Entity RdfType = Prefix.RDF + "type";
-        public static readonly Entity RdfFirst = Prefix.RDF + "first";
-        public static readonly Entity RdfRest = Prefix.RDF + "rest";
 
-        public static readonly Entity LogImplies = Prefix.LOG + "implies";
-	}
+    /// <summary>
+    /// Common RDF identifiers.
+    /// </summary>
+    public static class Identifier
+    {
+        public static readonly Entity RdfNil = Namespace.RDF + "nil";
+    }
+
+    /// <summary>
+    /// Common RDF predicates.
+    /// </summary>
+    public class Predicate
+    {
+        public static readonly Entity RdfType = Namespace.RDF + "type";
+        public static readonly Entity RdfFirst = Namespace.RDF + "first";
+        public static readonly Entity RdfRest = Namespace.RDF + "rest";
+
+        public static readonly Entity LogImplies = Namespace.LOG + "implies";
+    }
 }
